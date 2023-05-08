be89e9ce2c17699c1a2d47675750dfd4692efdf2
glihm <dev@glihm.net>
2023-05-08 17:23:47 -0600

    factorize supported languages

--- build.sh
+++ build.sh
@@ -3,7 +3,7 @@ echo building book
 mdbook build
 
 # Build es and fr versions.
-for po_lang in es fr
+for po_lang in $(cat ./LANGUAGES)
 do
     echo building "$po_lang"
     MDBOOK_BOOK__LANGUAGE="$po_lang" mdbook build -d book/"$po_lang"
